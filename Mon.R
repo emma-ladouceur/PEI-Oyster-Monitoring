74a3306bf7b675a5964f33530c53e2cdc1e6f687
Emma Ladouceur <emmaladouceur@Emmas-MacBook-Pro.local>
2024-10-03 11:40:37 -0300

    adding wd's

--- Mon.R
+++ Mon.R
@@ -5,8 +5,20 @@ library(tidyverse)
 library(ggplot2)
 library(viridis)
 
+# set your own personal working directory below
+# Emma MBP
 setwd("~/Dropbox/_Academic/Teaching/UPEI/Data/Oysters/")
+# Leah PC
+setwd("C://Users//lrmeister//OneDrive - University of Prince Edward Island//ACC 3100//Oyster Code")
+# Camille PC
+setwd("T:/Biodiversity Oyster Data")
+# Courtney PC
+setwd("C://Users//cmcbride13723//Desktop//R Code Oysters//Oysters")
+# Romy
 
+
+
+# load dataset you want
 mon <- read.csv("Oyster Monitoring Results 2013- present.csv", header= TRUE)
 
 head(mon)
@@ -24,10 +36,10 @@ mon_clean <- mon %>%  mutate( location_clean = case_when( location == "Savage Hb
 mutate( area_clean = case_when( location == "East River - MacWilliams Seafood" ~ 6,
                                  location == "Foxley - Gibb's Creek" ~ 1,
                                 location == "Foxley - Goff Bridge" ~ 1,
-                                location == "Foxley - Lot 6 Pt." ~ 1,
+                                location == "Foxley - Lot 6 Pt." ~ 1, 
                                 location == "Grand River" ~ 4,
                                 location == "Rustico" ~ 8,
-                                # we made decisions
+                                # we made decisions, created new areas for this section, and these locations
                                 location == "Dunk River" ~ 9,
                                 location == "Wilmot River" ~ 9,
                                 location_clean == "Pinette River" ~ 7,
@@ -66,7 +78,7 @@ mon_prep %>% select(month,day) %>% distinct() %>% arrange(month, day)
 View( mon_prep %>% select(year, month, day) %>% distinct() %>% arrange(year, month, day))
 summary(mon_prep)
 
-#
+# take the max
 max_l <- mon_prep %>% group_by(location_clean, f_year) %>% # group by location and year
   filter( larvae_above_250_microns == max(larvae_above_250_microns))  %>% # take max of larvae for each group specified above
   select(location_clean, area_clean, f_year, n_year, month_day, larvae_above_250_microns) %>% # select some columns
@@ -87,7 +99,7 @@ ggplot(data = max_l , aes( y= n_month_day, x = n_year,  group= location_clean, c
   # below we manipulate the x and y axis to tell it what to show
   scale_x_continuous( breaks=c(2012, 2014,  2016,  2018,  2020,  2022,  2024))+
   scale_y_continuous( limits = c(700,  825), breaks=c( 710,  725, 810, 825))+
-  # use a nice minimalistoverall theme format
+  # use a nice minimalist overall theme format
   theme_classic()
 
 # this time we use geom_smooth to fit a very simple linear model for every area overall
